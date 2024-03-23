411645f6e41b9249657f87b1f9d2e1c6657ae1d7
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-03-23 19:40:51 +0700

    [embedded-3-23-1940] complete automatic arch cmake libopencm and auto generate linker script

--- embedded/cmake/toolchain-arm-none-eabi-gcc.cmake
+++ embedded/cmake/toolchain-arm-none-eabi-gcc.cmake
@@ -2,7 +2,7 @@ set(CMAKE_SYSTEM_NAME                 Generic)
 set(CMAKE_SYSTEM_PROCESSOR            arm)
 
 # Toolchain prefix
-set(TOOLCHAIN_PREFIX "arm-none-eabi")
+set(TOOLCHAIN_PREFIX arm-none-eabi)
 
 # CC		:= $(PREFIX)-gcc
 # CXX		:= $(PREFIX)-g++
@@ -16,28 +16,21 @@ set(TOOLCHAIN_PREFIX "arm-none-eabi")
 # toolchain list
 set(CMAKE_C_COMPILER                  ${TOOLCHAIN_PREFIX}-gcc)
 set(CMAKE_CXX_COMPILER                ${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_LINKER                      ${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_AR                          ${TOOLCHAIN_PREFIX}-ar)
 set(CMAKE_ASM_COMPILER                ${TOOLCHAIN_PREFIX}-as)
 set(CMAKE_OBJCOPY                     ${TOOLCHAIN_PREFIX}-objcopy)
-set(CMAKE_DUMP                        ${TOOLCHAIN_PREFIX}-objdump)
 set(CMAKE_SIZE                        ${TOOLCHAIN_PREFIX}-size)
-set(CMAKE_RANLIB                      ${TOOLCHAIN_PREFIX}-ranlib)
-set(CMAKE_STRIP                       ${TOOLCHAIN_PREFIX}-strip)
 
 # flag list
-set(CMAKE_C_FLAGS                   "-Wno-psabi --specs=nosys.specs -fdata-sections -ffunction-sections -Wl,--gc-sections" CACHE INTERNAL "")
-set(CMAKE_CXX_FLAGS                 "${CMAKE_C_FLAGS} -fno-exceptions" CACHE INTERNAL "")
+set(CMAKE_C_FLAGS                     "-Wno-psabi --specs=nosys.specs -fdata-sections -ffunction-sections -Wl,--gc-sections" CACHE INTERNAL "")
+set(CMAKE_CXX_FLAGS                   "${CMAKE_C_FLAGS} -fno-exceptions " CACHE INTERNAL "")
 
-set(CMAKE_C_FLAGS                     ${FLAGS})
-set(CMAKE_CXX_FLAGS                   ${CPP_FLAGS})
-
-# cmake flag debug/release list
 set(CMAKE_C_FLAGS_DEBUG               "-Os -g" CACHE INTERNAL "")
 set(CMAKE_C_FLAGS_RELEASE             "-Os -DNDEBUG" CACHE INTERNAL "")
 set(CMAKE_CXX_FLAGS_DEBUG             "${CMAKE_C_FLAGS_DEBUG}" CACHE INTERNAL "")   
 set(CMAKE_CXX_FLAGS_RELEASE           "${CMAKE_C_FLAGS_RELEASE}" CACHE INTERNAL "") 
 
+# set this for compile into static library instead executable 
+set(CMAKE_TRY_COMPILE_TARGET_TYPE     STATIC_LIBRARY)
 
 set(CMAKE_EXECUTABLE_SUFFIX_C         .elf)
 set(CMAKE_EXECUTABLE_SUFFIX_CXX       .elf)
@@ -55,15 +48,8 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
-# set this for compile into static library instead executable 
-set(CMAKE_TRY_COMPILE_TARGET_TYPE     STATIC_LIBRARY)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-# ============================================
-# FUNCTION SECTIONS
-# ============================================
-# function to add custom linker to project 
 
 if(NOT WIN32)
   string(ASCII 27 Esc)
@@ -93,13 +79,9 @@ function(stm32_add_linker_script TARGET VISIBILITY SCRIPT)
     NORMALIZE
     OUTPUT_VARIABLE RELATIVE_SCRIPT)
 
-  # message("${TARGET} ${VISIBILITY} -T ${RELATIVE_SCRIPT}")
-
   target_link_options(${TARGET} ${VISIBILITY} -T ${RELATIVE_SCRIPT})
 
 endfunction()
-
-
 
 function(stm32_add_flash_targets TARGET)
     add_custom_target(${TARGET}-stlink-flash
@@ -112,7 +94,7 @@ function(stm32_add_flash_targets TARGET)
     )
 endfunction()
 
-function(generate_linker_script DEVICE)
+macro(generate_linker_script DEVICE)
   set(DEVICE_DATA "${libopencm3_SOURCE_DIR}/ld/devices.data")
 
   message("-- Generating Linker Script For ${DEVICE}")
@@ -150,27 +132,24 @@ function(generate_linker_script DEVICE)
     \t Defs     :  ${GENLINK_DEFS}" 
     )
 
-  string(APPEND CMAKE_CXX_FLAGS   ${GENLINK_CPPFLAGS})
-
-  string(APPEND ARCH_FLAGS " -mcpu=${GENLINK_CPU}")
-  # string(APPEND ARCH_FLAGS " --static -nostartfiles -fno-common")
+  list(APPEND ARCH_FLAGS  -mcpu=${GENLINK_CPU})
 
   if(${GENLINK_CPU} MATCHES "(cortex-m0|cortex-m0plus|cortex-m3|cortex-m4|cortex-m7)")
-    string(APPEND ARCH_FLAGS " -mthumb")
+    list(APPEND ARCH_FLAGS  -mthumb)
   endif()
   
   #==========================================================================
   if("${GENLINK_FPU}" STREQUAL "soft")
-    string(APPEND ARCH_FLAGS " -msoft-float")
+    list(APPEND ARCH_FLAGS  -msoft-float)
 
   elseif("${GENLINK_FPU}" STREQUAL "hard-fpv4-sp-d16")
-    string(APPEND ARCH_FLAGS " -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+    list(APPEND ARCH_FLAGS  -mfloat-abi=hard -mfpu=fpv4-sp-d16)
 
   elseif("${GENLINK_FPU}" STREQUAL "hard-fpv5-d16")
-    string(APPEND ARCH_FLAGS " -mfloat-abi=hard -mfpu=fpv4-sp-d16")
+    list(APPEND ARCH_FLAGS -mfloat-abi=hard -mfpu=fpv4-sp-d16)
 
   elseif("${GENLINK_FPU}" STREQUAL "hard-fpv5-sp-d16")
-    string(APPEND ARCH_FLAGS " -mfloat-abi=hard -mfpu=fpv5-sp-d16")
+    list(APPEND ARCH_FLAGS -mfloat-abi=hard -mfpu=fpv5-sp-d16)
 
   else()
     message(WARNING "warning No match for the FPU flags")
@@ -181,10 +160,29 @@ function(generate_linker_script DEVICE)
     message(WARNING "warning ${DEVICE} not found in ${DEVICE_DATA}")
   endif()
 
+  #==========================================================================
+  string(TOLOWER ${DEVICE} DEVICE_LOWER)
+  set(linker_s ${libopencm3_SOURCE_DIR}/ld/linker.ld.S)
+  set(custom_ld_script ${libopencm3_SOURCE_DIR}/ld/generated.${DEVICE_LOWER}.ld)
+  message("${BoldGreen}\n-- Generating linker script for ${DEVICE_LOWER}....${ColourReset}
+    \t output: ${custom_ld_script}")
+
+  message("${BoldGreen}-- Command To Generate Linker Script${ColourReset}")
+  string(REPLACE " " ";" GENLINK_DEFS_LIST ${GENLINK_DEFS})
+  execute_process(
+    COMMAND ${CMAKE_C_COMPILER} ${ARCH_FLAGS}  ${GENLINK_DEFS_LIST}  -P   -E  ${linker_s}  -o  ${custom_ld_script}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    COMMAND_ECHO  STDOUT)
+
+  #==========================================================================
+  set_property(TARGET auto_stm32 
+    PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libopencm3_SOURCE_DIR}/include)
 
   set_property(TARGET auto_stm32
-    # PROPERTY IMPORTED_LOCATION ${libopencm3_SOURCE_DIR}/lib/libopencm3_stm32f4.a)
     PROPERTY IMPORTED_LOCATION ${libopencm3_SOURCE_DIR}/lib/libopencm3_${GENLINK_FAMILY}.a)
+
+  add_dependencies(auto_stm32 libopencm3)
+  target_link_directories(auto_stm32 INTERFACE ${libopencm3_SOURCE_DIR}/lib)
 
   #==========================================================================
   # if(EXISTS "${libopencm3_SOURCE_DIR}/lib/libopencm3_${GENLINK_FAMILY}.a")
@@ -201,26 +199,20 @@ function(generate_linker_script DEVICE)
   # endif()
 
   #==========================================================================
+  
+  list(APPEND ARCH_FLAGS --static -nostartfiles -fno-common)
 
-  target_compile_definitions(auto_stm32 INTERFACE " -D${GENLINK_FAMILY}")
-  target_compile_options(auto_stm32 INTERFACE "${GENLINK_CPPFLAGS} ${ARCH_FLAGS}")
-  target_link_options(auto_stm32 INTERFACE "${GENLINK_CPPFLAGS} ${ARCH_FLAGS}")
+  target_compile_definitions(auto_stm32 INTERFACE ${GENLINK_CPPFLAGS})
+  target_compile_options(auto_stm32 INTERFACE  ${ARCH_FLAGS})
+  target_link_options(auto_stm32 INTERFACE ${ARCH_FLAGS})
 
-  string(TOLOWER "${DEVICE}" DEVICE_LOWER)
-  set(linker_s "${libopencm3_SOURCE_DIR}/ld/linker.ld.S")
-  set(ld_script "${libopencm3_SOURCE_DIR}/ld/generated.${DEVICE_LOWER}.ld")
-  set(ld_script_parent "${libopencm3_SOURCE_DIR}/ld/generated.${DEVICE_LOWER}.ld" PARENT_SCOPE)
-  message("${BoldGreen}\n-- Generating linker script for ${DEVICE_LOWER}....${ColourReset}
-    \t output: ${ld_script}")
+endmacro()
 
-  execute_process(
-    COMMAND "${CMAKE_C_COMPILER}" " -E  " " ${ARCH_FLAGS}" " ${GENLINK_DEFS}" " -P " " -E " ${linker_s} " -o " "${ld_script}"
-    OUTPUT_VARIABLE GENERATED_LD_SCRIPT)
-  message("${CMAKE_C_COMPILER}" " -E " " ${ARCH_FLAGS}" " ${GENLINK_DEFS}" " -P " " -E " ${linker_s} " -o " "${ld_script}")
-
-endfunction()
-
-# add new library from libopencm3
+#==========================================================================
+#
+# ADD NEW LIBRARY FROM LIBOPENCM3 AND GENERATE LINKER SCRIPT 
+#
+#==========================================================================
 message("${BoldGreen}-- Fetching libopencm3....${ColourReset}")
 
 include(FetchContent)
@@ -238,15 +230,8 @@ add_custom_target(libopencm3
 
 # Create a specific CPU target with the appropriate options etc
 add_library(auto_stm32 STATIC IMPORTED)
-set_property(TARGET auto_stm32 
-  PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${libopencm3_SOURCE_DIR}/include)
-
-
-add_dependencies(auto_stm32 libopencm3)
-target_link_directories(auto_stm32 INTERFACE ${libopencm3_SOURCE_DIR}/lib)
 
 message("${BoldGreen}========================================================================= ${ColourReset}")
 generate_linker_script(${auto_stm32_device})
 message("${BoldGreen}========================================================================= ${ColourReset}")
-
 
