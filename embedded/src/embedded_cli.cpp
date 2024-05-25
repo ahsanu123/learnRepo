3933b7c4801f99330bd6772675be9ce0d0fb7499
Ahsanu <caasperahsanuamala5@gmail.com>
2024-05-25 11:21:43 +0700

    [embedded-24-5-2024] embedded cli library working
    embedded cli not working when command is only 1, use at least 2 command
    to work

--- embedded/src/embedded_cli.cpp
+++ embedded/src/embedded_cli.cpp
@@ -18,11 +18,15 @@
  * along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include "cli_defs.h"
+#include <cli.h>
+#include <cstring>
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/usb/cdc.h>
 #include <libopencm3/usb/dwc/otg_fs.h>
 #include <libopencm3/usb/usbd.h>
+#include <stddef.h>
 #include <stdlib.h>
 
 static const struct usb_device_descriptor dev = {
@@ -165,13 +169,41 @@ static const char *usb_strings[] = {
     "DEMO",
 };
 
+usbd_device *usbd_dev;
+cli_t cli;
+
+// clang-format off
+static cli_status_t helpFunc(int argc, char **argv) {
+  cli.println("help function execute\r\n");
+  return CLI_OK;
+}
+static cli_status_t helloFunc(int argc, char **argv) {
+  cli.println("hello world!!!\r\n");
+  return CLI_OK;
+}
+// clang-format on
+
+cmd_t commandTable[] = {{
+                            .cmd = "help",
+                            .func = helpFunc,
+                        },
+                        {
+                            .cmd = "hello",
+                            .func = helloFunc,
+                        }};
+
+// clang-format off
 /* Buffer to be used for control requests. */
 uint8_t usbd_control_buffer[128];
 
-static enum usbd_request_return_codes cdcacm_control_request(
-    usbd_device *usbd_dev, struct usb_setup_data *req, uint8_t **buf,
+static enum usbd_request_return_codes cdcAcmControlRequest(
+    usbd_device *usbd_dev, 
+    struct usb_setup_data *req, 
+    uint8_t **buf,
     uint16_t *len,
-    void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req)) {
+    void (**complete)(usbd_device *usbd_dev, struct usb_setup_data *req)
+) {
+
   (void)complete;
   (void)buf;
   (void)usbd_dev;
@@ -185,7 +217,6 @@ static enum usbd_request_return_codes cdcacm_control_request(
      */
 
     /* We echo signals back to host as notification. */
-    usbd_ep_write_packet(usbd_dev, 0x83, "jangkrik boss", 13);
     return USBD_REQ_HANDLED;
   }
   case USB_CDC_REQ_SET_LINE_CODING:
@@ -197,33 +228,40 @@ static enum usbd_request_return_codes cdcacm_control_request(
   return USBD_REQ_HANDLED;
 }
 
-static void cdcacm_data_rx_cb(usbd_device *usbd_dev, uint8_t ep) {
+// clang-format on
+static void cdcAcmSendData(char *string) {
+  usbd_ep_write_packet(usbd_dev, 0x82, string, strlen(string));
+}
+
+static void cdcAcmDataRxCallback(usbd_device *usbd_dev, uint8_t ep) {
   (void)ep;
   char buf[64];
   int len = usbd_ep_read_packet(usbd_dev, 0x01, buf, 64);
 
   if (len) {
-    usbd_ep_write_packet(usbd_dev, 0x82, buf, len);
+    for (int i = 0; i < len; i++) {
+      cli_put(&cli, buf[i]);
+    }
+    /*usbd_ep_write_packet(usbd_dev, 0x82, buf, len);*/
   }
 }
 
-static void cdcacm_set_config(usbd_device *usbd_dev, uint16_t wValue) {
+static void cdcAcmSetConfig(usbd_device *usbd_dev, uint16_t wValue) {
   (void)wValue;
 
-  usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64, cdcacm_data_rx_cb);
+  usbd_ep_setup(usbd_dev, 0x01, USB_ENDPOINT_ATTR_BULK, 64,
+                cdcAcmDataRxCallback);
   usbd_ep_setup(usbd_dev, 0x82, USB_ENDPOINT_ATTR_BULK, 64, NULL);
   usbd_ep_setup(usbd_dev, 0x83, USB_ENDPOINT_ATTR_INTERRUPT, 16, NULL);
 
   usbd_register_control_callback(
       usbd_dev, USB_REQ_TYPE_CLASS | USB_REQ_TYPE_INTERFACE,
-      USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT, cdcacm_control_request);
+      USB_REQ_TYPE_TYPE | USB_REQ_TYPE_RECIPIENT, cdcAcmControlRequest);
 }
 
 int main(void) {
-  usbd_device *usbd_dev;
 
   rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
-  /*rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_84MHZ]);*/
 
   rcc_periph_clock_enable(RCC_GPIOA);
   rcc_periph_clock_enable(RCC_OTGFS);
@@ -244,10 +282,16 @@ int main(void) {
       sizeof(usbd_control_buffer));
 
   // clang-format on
+  usbd_register_set_config_callback(usbd_dev, cdcAcmSetConfig);
 
-  usbd_register_set_config_callback(usbd_dev, cdcacm_set_config);
+  cli.println = cdcAcmSendData;
+  cli.cmd_tbl = commandTable;
+  cli.cmd_cnt = sizeof(commandTable) / sizeof(cmd_t);
+
+  cli_init(&cli);
 
   while (1) {
     usbd_poll(usbd_dev);
+    cli_process(&cli);
   }
 }
