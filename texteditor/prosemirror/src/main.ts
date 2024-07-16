bbe1e07c0353254711db72dffec0af6d4f22d284
Ahsanu <caasperahsanuamala5@gmail.com>
2024-07-17 04:32:05 +0700

    add text editor learn

---
+++ texteditor/prosemirror/src/main.ts
@@ -0,0 +1,27 @@
+import './style.scss'
+import { schema } from 'prosemirror-schema-basic'
+import { EditorState } from 'prosemirror-state'
+import { EditorView } from 'prosemirror-view'
+
+declare global {
+  interface Window {
+    view: EditorView
+  }
+}
+
+const app = document.querySelector<HTMLDivElement>('#app')!
+
+const state = EditorState.create({ schema })
+const view = new EditorView(app, {
+  state: state,
+
+  dispatchTransaction: function(transaction) {
+    console.log("Document size went from", transaction.before.content.size,
+      "to", transaction.doc.content.size)
+    let newState = view.state.apply(transaction)
+    view.updateState(newState)
+  }
+
+})
+
+window.view = view;
