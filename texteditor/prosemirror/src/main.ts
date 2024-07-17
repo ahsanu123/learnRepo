ab859f6d0515b85053ffc6e41122de7c3c140c5f
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-07-17 12:17:51 +0700

    stupid worked from example

--- texteditor/prosemirror/src/main.ts
+++ texteditor/prosemirror/src/main.ts
@@ -1,9 +1,11 @@
+
 import './style.scss'
 import { schema } from 'prosemirror-schema-basic'
 import { EditorState } from 'prosemirror-state'
-import { EditorView } from 'prosemirror-view'
-import { undo, redo, history } from 'prosemirror-history'
-import { keymap } from 'prosemirror-keymap'
+import { EditorView, NodeView } from 'prosemirror-view'
+import { Node as ProseNode, Schema } from 'prosemirror-model'
+import { addListNodes } from 'prosemirror-schema-list'
+import { exampleSetup } from 'prosemirror-example-setup'
 
 declare global {
   interface Window {
@@ -11,28 +13,37 @@ declare global {
   }
 }
 
+class ImageView implements NodeView {
+  dom: HTMLImageElement
+
+  constructor(node: ProseNode) {
+    // The editor will use this as the node's DOM representation
+    this.dom = document.createElement("img")
+    this.dom.src = node.attrs.src
+    this.dom.addEventListener("click", e => {
+      console.log("You clicked me!")
+      e.preventDefault()
+    })
+  }
+  stopEvent() { return true }
+}
+
 const app = document.querySelector<HTMLDivElement>('#app')!
+
+const mySchema = new Schema({
+  nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
+  marks: schema.spec.marks
+})
 
 const state = EditorState.create({
   schema: schema,
-  plugins: [
-    history(),
-    keymap({
-      'Mod-z': undo,
-      'Mod-y': redo
-    })
-  ]
+  plugins: exampleSetup({
+    schema: mySchema,
+    menuBar: false
+  })
 })
 const view = new EditorView(app, {
   state: state,
-
-  dispatchTransaction: function(transaction) {
-    console.log("Document size went from", transaction.before.content.size,
-      "to", transaction.doc.content.size)
-    let newState = view.state.apply(transaction)
-    view.updateState(newState)
-  },
-
 })
 
 window.view = view;
