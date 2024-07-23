023e225d3252634407cc868dc8b072a5c1853c50
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-07-23 17:57:01 +0700

    add change color

--- kotlin/app/src/main/kotlin/Application/CenterCircleApp.kt
+++ kotlin/app/src/main/kotlin/Application/CenterCircleApp.kt
@@ -4,6 +4,8 @@ import javafx.application.Application
 import javafx.beans.binding.Bindings
 import javafx.scene.Group
 import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.HBox
 import javafx.scene.paint.Color
 import javafx.scene.shape.Circle
 import javafx.stage.Stage
@@ -15,8 +17,21 @@ class CenterCircle : Application() {
 
     override fun start(stage: Stage) {
         var circle = Circle()
-        circle.fill = Color.BLUE
+        var toBlue = Button("Change To Blue")
+        var toRed = Button("Change To Red")
         var root = Group(circle)
+        var buttonBar = HBox(10.0)
+
+        circle.fill = Color.BLUE
+        toRed.setOnAction { event ->
+            circle.fill = Color.RED
+        }
+        toBlue.setOnAction { event ->
+            circle.fill = Color.BLUE
+        }
+
+        buttonBar.getChildren().addAll(toRed, toBlue)
+        root.getChildren().addAll(buttonBar)
 
         stage.run {
             title = "Center Circle with Bindings"
