d75e6fee92f0525937434cbceb4e02fdbee68dba
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-08-01 11:32:51 +0700

    nit

--- erpPlanner/api/Migration/MasterDbContext.cs
+++ erpPlanner/api/Migration/MasterDbContext.cs
@@ -9,12 +9,5 @@ public class MasterContext : IdentityDbContext<CustomIdentityModel>
     public MasterContext(DbContextOptions<MasterContext> option)
         : base(option) { }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseNpgsql(
-            "Host=localhost;Port=5432;Username=ah;Password=;Database=planerp;"
-        );
-    }
-
     public DbSet<Storage> storage { get; set; }
 }
