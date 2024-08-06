3be82371abc52e4eb7ab4cf4130eafbe437533b6
Ahsanu Amala <caasperahsanuamala5@gmail.com>
2024-08-06 15:47:23 +0700

    slickgrid typescript not working

---
+++ slickgrid-uni/src/model/project-model.ts
@@ -0,0 +1,10 @@
+
+export interface ProjectModel {
+  id: string;
+  title: string;
+  duration: string;
+  percentComplete: number;
+  start: string;
+  finish: string;
+  effortDriven: boolean;
+}
