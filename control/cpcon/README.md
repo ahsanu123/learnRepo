279fc49cc78c5ae8be12842af546b72bd86c04cd
Ahsanu <caasperahsanuamala5@gmail.com>
2024-08-24 20:34:07 +0700

    save the progress

---
+++ control/cpcon/README.md
@@ -0,0 +1,11 @@
+# Introduction
+
+# Environment Setup
+
+# Interesting Library 
+
+**Machine Learning**
+
+- [Flashlight](https://github.com/flashlight/flashlight)
+- [MlPack](https://github.com/mlpack/mlpack)
+
--- control/cpcon/src/main.cpp
+++ control/cpcon/src/main.cpp
@@ -1,9 +1,21 @@
+#include "FL/Fl_Box.H"
+#include "FL/Fl_Window.H"
 #include <FL/Fl.H>
-#include <FL/Fl_Box.H>
-#include <FL/Fl_Window.H>
+#include <cmath>
+#include <matplot/matplot.h>
+
+using namespace matplot;
 
 int main(int argc, char **argv) {
-  Fl_Window *window = new Fl_Window(300, 180);
+  fplot([](double x) { return exp(x); }, std::array<double, 2>{-3, 0}, "b");
+  hold(on);
+  fplot([](double x) { return cos(x); }, std::array<double, 2>{0, 3}, "b");
+  hold(off);
+  grid(on);
+
+  show();
+
+  auto *window = new Fl_Window(300, 180);
   Fl_Box *box = new Fl_Box(20, 40, 260, 100, "Hello, World!");
   box->box(FL_UP_BOX);
   box->labelsize(36);
@@ -12,4 +24,5 @@ int main(int argc, char **argv) {
   window->end();
   window->show(argc, argv);
   return Fl::run();
+  return 0;
 }
